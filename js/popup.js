11b8d1f3a38cfa8cdd9c717cc63dc613fed067c1
alexey-rgb <54808066+alexey-rgb@users.noreply.github.com>
2019-10-23 19:30:28 +0300

    каталог ПП

--- js/popup.js
+++ js/popup.js
@@ -1,18 +1,19 @@
-var c = document.querySelector(".write-us-button");
-var b = document.querySelector(".container-popup");
-var a = document.querySelector(".close-popup");
+var button = document.querySelector(".write-us-button");
+var popup = document.querySelector(".container-popup");
+var closeform = document.querySelector(".close-popup");
 var d = document.querySelector("[name=name]");
 var e = document.querySelector("[name=e-mail]");
-c.addEventListener("click", function (evt) {
+popup.classList.remove("show-popup");
+button.addEventListener("click", function (evt) {
   evt.preventDefault();
-  b.classList.add("show-popup");
+  popup.classList.add("show-popup");
   d.focus();
   if (d.value) {
     evt.preventDefault();
     e.focus();
   }
 });
-a.addEventListener("click", function (evt) {
+closeform.addEventListener("click", function (evt) {
   evt.preventDefault();
-  b.classList.remove("show-popup");
+  popup.classList.remove("show-popup");
 });
