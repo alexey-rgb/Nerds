69e090a007e250b11265815f5d8792873f6244d8
Алексей <askerbl67@gmail.com>
2019-11-01 22:34:11 +0300

    на проверку

---
+++ js/map.js
@@ -0,0 +1,22 @@
+ymaps.ready(function () {
+  var myMap = new ymaps.Map('map', {
+      center: [59.939161, 30.321783],
+      zoom: 17
+    }, {
+      searchControlProvider: 'yandex#search'
+    }),
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+    ),
+    myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
+      hintContent: 'Собственный значок метки',
+      balloonContent: 'Это красивая метка'
+    }, {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/map-marker.png',
+      iconImageSize: [231, 190],
+      iconImageOffset: [-20, -220]
+    });
+  myMap.geoObjects
+    .add(myPlacemark);
+});
