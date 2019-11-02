2ef80fa2ae0dbb24d92215f3c2b6ff2cee7c3e8a
Алексей <askerbl67@gmail.com>
2019-11-02 23:19:10 +0300

    попап потрясти форму не срабатывает посмотри пожалуйста

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,22 @@
 var button_show_popup = document.querySelector(".write-us-button");
 var popup = document.querySelector(".container-popup");
 var close_form = document.querySelector(".close-popup");
-var input_name = document.querySelector("[name=name]");
+var name = document.querySelector("[name=text]");
+var email = document.querySelector("[name=email]");
+var form = document.querySelector("form");
 popup.classList.remove("show-popup");
 button_show_popup.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("show-popup");
-  input_name.focus();
+  name.focus();
 });
 close_form.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("show-popup");
+});
+form.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  if (!email.value || !name.value){
+ name.classList.add("shake-form");
+}
 });
