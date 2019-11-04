89e4090723ffb2d2d6e6eb867daa278472eff36e
Алексей <askerbl67@gmail.com>
2019-11-04 14:31:45 +0300

    попытка3

--- js/popup.js
+++ js/popup.js
@@ -2,8 +2,6 @@ var button_show_popup = document.querySelector(".write-us-button");
 var popup = document.querySelector(".container-popup");
 var close_form = document.querySelector(".close-popup");
 var name = document.querySelector("[name=name]");
-var email = document.querySelector("[name=email]");
-var form = document.querySelector("form");
 popup.classList.remove("show-popup");
 button_show_popup.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -13,11 +11,4 @@ button_show_popup.addEventListener("click", function (evt) {
 close_form.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("show-popup");
-});
-form.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-  if (!email || !name){
-    popup.classList.remove("shake-form");
-    popup.classList.add("shake-form");
-  }
 });
